d980c1fc230a26602625966f6ad2ce1331978ec4
MD ISMAIL HOSEN <43248620+ismail61@users.noreply.github.com>
2021-11-23 10:43:13 -0800

    Add files via upload
    Validation

--- js/script.js
+++ js/script.js
@@ -1,19 +1,44 @@
+//Email Validation
+const validMail = (mail) => {
+    if (/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(mail)) {
+        return true
+    }
+    return false
+}
+
+const errMsg = (node, msg) => {
+    if (msg && node) {
+        node.innerText = msg
+        setTimeout(() => {
+            node.innerText = ''
+        }, 2000)
+    }
+}
+
 //Sign Up
 
 const SignUp = e => {
     try {
         e.preventDefault();
+        let err = document.getElementById('alert')
         let user = {
             name: document.getElementById('name').value,
             email: document.getElementById('email').value,
             password: document.getElementById('password').value
         }
-        if (user) {
-            let users = new Array()
-            users = JSON.parse(localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : []
-            users.push(user)
-            localStorage.setItem('users', JSON.stringify(users));
-
+        if (user.name.length < 6) {
+            errMsg(err, 'Name must be 6 characters')
+        } else if (user.password.length < 6) {
+            errMsg(err, 'Password must be 6 characters')
+        } else if (!validMail(user.email)) {
+            errMsg(err, 'Please provide valid Email Address')
+        } else {
+            if (user) {
+                let users = new Array()
+                users = JSON.parse(localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : []
+                users.push(user)
+                localStorage.setItem('users', JSON.stringify(users));
+            }
         }
     } catch (error) {
         console.log(error.message)
@@ -25,20 +50,28 @@ SignUp();
 //Sign In
 const SignIn = e => {
     e.preventDefault();
+    let err = document.getElementById('alert')
     let user = {
         email: document.getElementById('email').value,
         password: document.getElementById('password').value
     }
     if (user) {
-        const getUsers = JSON.parse(localStorage.getItem('users'))
-        let ok = false
-        for (let getUser of getUsers) {
-            if (getUser.email === user.email && getUser.password === user.password) {
-                ok = true
-                break
-            } 
+        if (!validMail(user.email)) {
+            errMsg(err, 'Please provide valid Email Address')
+        } else if (user.password.length < 6) {
+            errMsg(document.getElementById('alert'), 'Password must be 6 characters')
+        } else {
+            const getUsers = JSON.parse(localStorage.getItem('users'))
+            let ok = false
+            for (let getUser of getUsers) {
+                if (getUser.email === user.email && getUser.password === user.password) {
+                    ok = true
+                    break
+                }
+            }
+            ok ? console.log("Successfully Sign In") : console.log("Invalid Credentials")
         }
-        ok?console.log("Successfully Sign In"):console.log("Invalid Credentials")
+
     }
 }
 
