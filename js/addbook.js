67bcfbbe9632fe080b1bcd555685543b0ece39dc
MD ISMAIL HOSEN <43248620+ismail61@users.noreply.github.com>
2021-11-25 22:24:17 -0800

    Add files via upload

--- js/addbook.js
+++ js/addbook.js
@@ -1,43 +1,66 @@
+const errMsg = (node, msg) => {
+    if (msg && node) {
+        node.innerText = msg
+        setTimeout(() => {
+            node.innerText = ''
+        }, 4000)
+    }
+}
+
+
+
 
 //Add Book
 const addBook = async e => {
+    let err = document.getElementById('alert')
     e.preventDefault();
     let book = {
         title: document.getElementById('title').value,
         author: document.getElementById('author').value,
         isbn: document.getElementById('isbn').value,
-        price : document.getElementById('price').value
+        price: document.getElementById('price').value
     }
-    const imgPath = document.querySelector('input[type=file]').files[0];
-    const reader = new FileReader();
-
-    reader.addEventListener("load", () => {
-        // convert image file to base64 string and save to localStorage
-        let books = new Array()
-        books = JSON.parse(localStorage.getItem('books')) ? JSON.parse(localStorage.getItem('books')) : []
-        books.push({
-            title: book.title,
-            author: book.author,
-            isbn: book.isbn,
-            price : book.price,
-            image: reader.result
+    let ok = true
+    let books = JSON.parse(localStorage.getItem('books')) ? JSON.parse(localStorage.getItem('books')) : []
+    if (books) {
+        books.map(oldbook => {
+            if (oldbook.isbn == book.isbn) {
+                errMsg(err, 'ISBN Number must be Unique')
+                ok = false
+            }
         })
-        localStorage.setItem("books", JSON.stringify(books));
-        document.getElementById('title').value = ''
-        document.getElementById('author').value = ''
-        document.getElementById('isbn').value = ''
-        document.getElementById('coverimage').value =''
-        document.getElementById('price').value = ''
-        document.getElementById('successful').innerHTML = 'New Book added Successful'
-        setTimeout(() => {
-            document.getElementById('successful').innerHTML = ''
-        }, 2000)
-         
-    }, false);
-
-    if (imgPath) {
-        reader.readAsDataURL(imgPath);
     }
+    if (ok) {
+        const imgPath = document.querySelector('input[type=file]').files[0];
+        const reader = new FileReader();
 
+        reader.addEventListener("load", () => {
+            // convert image file to base64 string and save to localStorage
+            let books = new Array()
+            books = JSON.parse(localStorage.getItem('books')) ? JSON.parse(localStorage.getItem('books')) : []
+            books.push({
+                title: book.title,
+                author: book.author,
+                isbn: book.isbn,
+                price: book.price,
+                image: reader.result
+            })
+            localStorage.setItem("books", JSON.stringify(books));
+            document.getElementById('title').value = ''
+            document.getElementById('author').value = ''
+            document.getElementById('isbn').value = ''
+            document.getElementById('coverimage').value = ''
+            document.getElementById('price').value = ''
+            document.getElementById('successful').innerHTML = 'New Book added Successful'
+            setTimeout(() => {
+                document.getElementById('successful').innerHTML = ''
+            }, 2000)
+
+        }, false);
+
+        if (imgPath) {
+            reader.readAsDataURL(imgPath);
+        }
+
+    }
 }
-
--- js/showbooks.js
+++ js/showbooks.js
@@ -1,10 +1,10 @@
-(function () {
+const showBooks = () => {
     let books = JSON.parse(localStorage.getItem('books')) ? JSON.parse(localStorage.getItem('books')) : []
     if (books) {
         let allBooks = document.getElementById('books')
         books.map(book => {
             let html = `
-                <div class="col-xs-12 col-sm-6 col-md-4" style="width : 19rem">
+                <div class="col-xs-12 col-sm-6 col-md-4" style="width : 18rem">
                     <div class="card">
                         <a class="img-card" href="">
                             <img
@@ -27,8 +27,8 @@
                             <div class="buy d-flex justify-content-between align-items-center">
                                 <div>$ ${book.price}</div>
                                 <div>
-                                    <i title="Edit" class="far fa-edit m-1 text-success edit"></i>
-                                    <i title="Delete" class="fas fa-trash-alt m-1 text-danger delete"></i>
+                                    <i title="Edit" onclick="Edit(${book.isbn})" class="far fa-edit m-1 text-success edit"></i>
+                                    <i title="Delete" onclick="Delete(${book.isbn})" class="fas fa-trash-alt m-1 text-danger delete"></i>
                                 </div>
                             </div>       
                         </div>
@@ -40,5 +40,27 @@
     } else {
 
     }
-})();
+}
+window.onload = showBooks()
 
+//Edit a Book
+const Edit = isbn => {
+    console.log(isbn)
+}
+Edit();
+
+//Delete a book
+const Delete = isbn => {
+    if (isbn) {
+        if (confirm('Are you sure you want to delete this?')) {
+            let books = JSON.parse(localStorage.getItem('books'))
+            var newBooks = books.filter(book => {
+                return book.isbn != isbn;
+            })
+            localStorage.removeItem('books')
+            localStorage.setItem('books',JSON.stringify(newBooks))
+            window.open('../LibaryManagementSystem/showbooks.html','_self')
+        }
+    }
+}
+Delete();
